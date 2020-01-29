15c356a8aa045f8aca03e39a3314d1115c5f5e39
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-01-28 16:56:08 -0800

    basic styling and basic version completed

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { delay, filter, scan } from "rxjs/operators";
+import { MathValidators } from "../math-validators";
 
 @Component({
-  selector: 'app-equation',
-  templateUrl: './equation.component.html',
-  styleUrls: ['./equation.component.css']
+  selector: "app-equation",
+  templateUrl: "./equation.component.html",
+  styleUrls: ["./equation.component.css"]
 })
 export class EquationComponent implements OnInit {
+  secondPerSolution = 0;
+  mathForm = new FormGroup(
+    {
+      a: new FormControl(this.randomNumber()),
+      b: new FormControl(this.randomNumber()),
+      answer: new FormControl("")
+    },
+    [MathValidators.addition("answer", "a", "b")]
+  );
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  get a() {
+    return this.mathForm.value.a;
   }
 
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  ngOnInit() {
+    // const startTime = new Date();
+    // let numberSolved = 0;
+
+    this.mathForm.statusChanges
+      .pipe(
+        filter(el => el === "VALID"),
+        delay(250),
+        scan(
+          (acc, value) => {
+            return {
+              numberSolved: acc.numberSolved + 1,
+              startTime: acc.startTime
+            };
+          },
+          { numberSolved: 0, startTime: new Date() }
+        )
+      )
+      .subscribe(({ numberSolved, startTime }) => {
+        // numberSolved += 1;
+        this.secondPerSolution =
+          (new Date().getTime() - startTime.getTime()) / numberSolved / 1000;
+
+        this.mathForm.setValue({
+          a: this.randomNumber(),
+          b: this.randomNumber(),
+          answer: ""
+        });
+      });
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
 }
