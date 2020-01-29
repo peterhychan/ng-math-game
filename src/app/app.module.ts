c046adc6373186d7313251b0f7fd21369633bd27
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-01-29 12:38:47 -0800

    adjusted styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { EquationComponent } from "./equation/equation.component";
 import { AnswerHighlightDirective } from './answer-highlight.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, EquationComponent, AnswerHighlightDirective],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
